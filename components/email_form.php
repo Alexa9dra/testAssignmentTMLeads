dda8d51a4061ee88abeb010ce1345f317b55b576
Oleksandra Rohach <rohacho2020@gmail.com>
2025-03-13 15:05:56 +0200

    Delete components directory

--- components/email_form.php
+++
@@ -1,6 +0,0 @@
-<form id="email-form">
-    <input placeholder="Ваш e–mail" />
-    <button type="submit"> 
-        <?php echo isset($buttonLabel) ? htmlspecialchars($buttonLabel) : "Submit"; ?>
-    </button>
-</form>
